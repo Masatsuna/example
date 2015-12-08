a36a1eace9cb9981d36ad2d169eeb3ae2501c0f3
Masatsuna <CloQuespayl6>
2015-12-08 19:56:06 +0900

    test2

--- app/src/main/java/org/t_robop/masatsuna/example/MainActivity.java
+++ app/src/main/java/org/t_robop/masatsuna/example/MainActivity.java
@@ -2,6 +2,7 @@ package org.t_robop.masatsuna.example;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("", "");
     }
 
     public void click(View view){
